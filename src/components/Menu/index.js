2539f3f4d5f68f411b175ded20bcfb9c988c2614
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-11 14:09:15 +0200

    open menu functionality with the open button.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import OpenButton from './OpenButton/index'
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuIsOpen: false
+        }
+
+        this.openMenu = this.openMenu.bind(this);
+    }
+
+    openMenu = () => {
+        this.setState({
+            menuIsOpen: !this.state.menuIsOpen
+        });
+    }
+
+
     render() {
         return (
             <div className="menu-wrapper">
-                <OpenButton></OpenButton>
-                <div className="menu-background">
-                    lalla
+                <OpenButton open={this.state.menuIsOpen} onClick={this.openMenu}></OpenButton>
+                <div className={this.state.menuIsOpen ? 'menu-background open' : 'menu-background'}>
+                    
                 </div>
             </div>
             
