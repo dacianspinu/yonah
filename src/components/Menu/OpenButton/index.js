786264b36406952e8796f6c7aac609b653322c78
Dacian Spînu <macbook@Macs-MacBook-Air.local>
2017-01-11 14:10:06 +0200

    openButton functionality to handle the open/closed state of the menu.

--- src/components/Menu/OpenButton/index.js
+++ src/components/Menu/OpenButton/index.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './index.css';
 
 class OpenButton extends Component {
+    constructor(props) {
+        super(props);
+
+        this.changeButton = this.changeButton.bind(this);
+    }
+
+    changeButton = () => {
+        this.props.onClick();
+    }
     render() {
         return(
-            <div className="open-button">
-                <div className="burger-icon-wrapper">
-                    <div className="burger-line"></div>
-                    <div className="burger-line"></div>
-                    <div className="burger-line"></div>
+            <div className="open-button" onClick={this.changeButton}>
+                <div className={this.props.open ? 'burger-icon-wrapper close' : 'burger-icon-wrapper'}>
+                    <div className="burger-line first-line"></div>
+                    <div className="burger-line second-line"></div>
+                    <div className="burger-line third-line"></div>
                 </div>
             </div>
         );
